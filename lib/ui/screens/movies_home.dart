9229da42b18a149f42e6aa7df8b1f3a3ac60edb3
vedran.ivic <vedran.ivic@atos.net>
2022-11-08 23:23:04 +0100

    Offline notification widgets extracting.

--- lib/ui/screens/movies_home.dart
+++ lib/ui/screens/movies_home.dart
@@ -3,8 +3,6 @@
  * This file is part of movies_app Flutter application project.
  */
 
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/common/colors.dart';
@@ -14,6 +12,7 @@ import '../../blocs/movies_bloc/movies_bloc.dart';
 import '../widgets/favourites_tab_view.dart';
 import '../widgets/home_bottom_navigation_bar.dart';
 import '../widgets/movies_tab_view.dart';
+import '../widgets/offline_notification_icon.dart';
 
 class MoviesHome extends StatefulWidget {
   const MoviesHome({Key? key}) : super(key: key);
@@ -36,20 +35,7 @@ class _MoviesHomeState extends State<MoviesHome> {
     return BlocListener<ConnectivityBloc, ConnectivityState>(
       listener: (context, state) {
         if(state is ConnectivityDisConnected){
-          ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Row(
-                  children: const [
-                    Icon(
-                      Icons.wifi_off,
-                      color: appTextColor,
-                    ),
-                    SizedBox(width: 8,),
-                    Text("You are currently offline"),
-                  ],
-                ),
-              )
-          );
+          _showOfflineNotification();
         }
       },
       child: DefaultTabController(
@@ -75,18 +61,7 @@ class _MoviesHomeState extends State<MoviesHome> {
                           height: 28,
                           width: 28,
                         ),
-                        BlocBuilder<ConnectivityBloc, ConnectivityState>(
-                          builder: (context, state) {
-                            if(state is ConnectivityDisConnected) {
-                              return const Icon(
-                                Icons.wifi_off,
-                                color: primaryFaded,
-                                size: 28,
-                              );
-                            }
-                            return const SizedBox();
-                          },
-                        )
+                        const OfflineNotificationIcon()
                       ],
                     ),
                   ),
@@ -105,6 +80,23 @@ class _MoviesHomeState extends State<MoviesHome> {
           ),
         ),
       ),
+    );
+  }
+
+  void _showOfflineNotification() {
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Row(
+            children: const [
+              Icon(
+                Icons.wifi_off,
+                color: appTextColor,
+              ),
+              SizedBox(width: 8,),
+              Text("You are currently offline"),
+            ],
+          ),
+        )
     );
   }
 }
